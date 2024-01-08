73386d4eda867d3c8acaac9cacbd047f78f2728e
Vansh Bataviya <bataviyavansh@gmail.com>
2024-01-07 20:54:13 -0500

    Amended create-react-app with Figma v1.0

---
+++ src/components/Desktop1.tsx
@@ -0,0 +1,156 @@
+export default function Desktop1({ className = "" }: Desktop1Props) {
+  return (
+    <div className="font-josefin_sans inline-flex h-[2257px] w-[1422px] resize flex-row overflow-hidden text-left font-normal leading-[normal] tracking-[0px]" >
+      <div className="flex h-full w-full flex-wrap items-end overflow-clip bg-orange-50 pb-16" >
+        <div className="flex w-[1645px] items-start justify-center self-stretch pb-[831px]" >
+          <div className="relative flex flex-grow items-start self-stretch pb-56 pl-20 pr-36 pt-10" >
+            <div className="absolute left-0 top-0 h-80 w-80">
+              <svg width="100%" height="100%" style={{"overflow":"visible"}} preserveAspectRatio="none" viewBox="-89 -41 345 331" fill="none" xmlns="http://www.w3.org/2000/svg"><g filter="url(#filter0_f_7_11)"><ellipse cx="83.5" cy="124.5" rx="172.5" ry="165.5" fill="white"/></g><defs><filter id="filter0_f_7_11" x="-109" y="-61" width="385" height="371" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/><feGaussianBlur stdDeviation="10" result="effect1_foregroundBlur_7_11"/></filter></defs></svg>
+            </div>
+            <div className="absolute right-0 top-0 h-80 w-80">
+              <svg width="100%" height="100%" style={{"overflow":"visible"}} preserveAspectRatio="none" viewBox="20 -41 345 331" fill="none" xmlns="http://www.w3.org/2000/svg"><g filter="url(#filter0_f_7_12)"><ellipse cx="192.5" cy="124.5" rx="172.5" ry="165.5" fill="white"/></g><defs><filter id="filter0_f_7_12" x="0" y="-61" width="385" height="371" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/><feGaussianBlur stdDeviation="10" result="effect1_foregroundBlur_7_12"/></filter></defs></svg>
+            </div>
+            <div className="absolute left-96 top-96 h-96 w-96">
+              <svg width="100%" height="100%" style={{"overflow":"visible"}} preserveAspectRatio="none" viewBox="20 20 494 455" fill="none" xmlns="http://www.w3.org/2000/svg"><g filter="url(#filter0_f_7_15)"><ellipse cx="267" cy="247.5" rx="247" ry="227.5" fill="white"/></g><defs><filter id="filter0_f_7_15" x="0" y="0" width="534" height="495" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/><feGaussianBlur stdDeviation="10" result="effect1_foregroundBlur_7_15"/></filter></defs></svg>
+            </div>
+            <div className="absolute bottom-60 right-12 h-80 w-80">
+              <svg width="100%" height="100%" style={{"overflow":"visible"}} preserveAspectRatio="none" viewBox="20 20 345 331" fill="none" xmlns="http://www.w3.org/2000/svg"><g filter="url(#filter0_f_7_14)"><ellipse cx="192.5" cy="185.5" rx="172.5" ry="165.5" fill="white"/></g><defs><filter id="filter0_f_7_14" x="0" y="0" width="385" height="371" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/><feGaussianBlur stdDeviation="10" result="effect1_foregroundBlur_7_14"/></filter></defs></svg>
+            </div>
+            <div className="absolute bottom-64 left-0 h-80 w-80">
+              <svg width="100%" height="100%" style={{"overflow":"visible"}} preserveAspectRatio="none" viewBox="-89 20 345 331" fill="none" xmlns="http://www.w3.org/2000/svg"><g filter="url(#filter0_f_7_13)"><ellipse cx="83.5" cy="185.5" rx="172.5" ry="165.5" fill="white"/></g><defs><filter id="filter0_f_7_13" x="-109" y="0" width="385" height="371" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/><feGaussianBlur stdDeviation="10" result="effect1_foregroundBlur_7_13"/></filter></defs></svg>
+            </div>
+            <div className="relative flex flex-grow flex-col items-center gap-y-24 self-stretch bg-orange-50/[0.93] pb-72 pl-12 pr-8 pt-10 text-[40px] leading-[normal] text-yellow-800" >
+              <div className="ml-2.5 h-16 flex-wrap gap-y-7 gap-x-2 self-stretch rounded-[60px] pl-7 pr-6 pt-2.5" >
+                <svg width="100%" height="100%" style={{"overflow":"visible"}} preserveAspectRatio="none" viewBox="2 0 1328 68" fill="none" xmlns="http://www.w3.org/2000/svg"><g filter="url(#filter0_d_1_4)"><rect x="2" width="1328" height="68" rx="34" fill="#DDB892"/></g><defs><filter id="filter0_d_1_4" x="0" y="0" width="1332" height="74" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/><feOffset dy="4"/><feGaussianBlur stdDeviation="1"/><feComposite in2="hardAlpha" operator="out"/><feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/><feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_4"/><feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_4" result="shape"/></filter></defs></svg>
+              </div>
+              <div className="flex items-center justify-center pr-3">
+                <div className="relative flex flex-grow items-start justify-end self-stretch pb-24 pl-64 pr-60" >
+                  <div className="absolute inset-x-0 bottom-5 h-16">
+                    <svg width="100%" height="100%" style={{"overflow":"visible"}} preserveAspectRatio="none" viewBox="0 0 1004 64" fill="none" xmlns="http://www.w3.org/2000/svg"><ellipse cx="502" cy="32" rx="502" ry="32" fill="url(#paint0_linear_7_18)"/><defs><linearGradient id="paint0_linear_7_18" x1="502" y1="0" x2="502" y2="64" gradientUnits="userSpaceOnUse"><stop stop-color="#DDB892" stop-opacity="0"/><stop offset="1" stop-color="#DDB892"/></linearGradient></defs></svg>
+                  </div>
+                  <div className="bg-b-bg-ellipse-7bbpolygon-1bbpolygon-2 absolute bottom-0 right-96 flex h-9 w-10 items-center justify-center bg-cover bg-center py-3 px-1.5" >
+                    <div className="flex flex-grow flex-wrap items-center justify-center gap-y-0 gap-x-0 pt-0" >
+                      <div className="h-3 w-3.5 self-stretch">
+                        <svg width="100%" height="100%" style={{"overflow":"visible"}} preserveAspectRatio="none" viewBox="0 0 14 12.23" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.0509686 6.05811L10.5187 0.899343L10.4304 11.3923L0.0509686 6.05811Z" fill="white"/></svg>
+                      </div>
+                      <div className="h-3 w-3.5 self-stretch">
+                        <svg width="100%" height="100%" style={{"overflow":"visible"}} preserveAspectRatio="none" viewBox="-3 0 13.91 12.13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.9041 6.05831L0.484896 11.3144L0.475384 0.821109L10.9041 6.05831Z" fill="white"/></svg>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="relative h-80 w-96">
+                    <svg width="100%" height="100%" style={{"overflow":"visible"}} preserveAspectRatio="none" viewBox="4 4 498 325" fill="none" xmlns="http://www.w3.org/2000/svg"><g filter="url(#filter0_f_7_26)"><ellipse cx="253" cy="166.5" rx="249" ry="162.5" fill="white" fill-opacity="0.04"/></g><defs><filter id="filter0_f_7_26" x="0" y="0" width="506" height="333" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/><feGaussianBlur stdDeviation="2" result="effect1_foregroundBlur_7_26"/></filter></defs></svg>
+                  </div>
+                  <img
+                    className="absolute bottom-6 right-32 h-72 w-32 object-cover object-center drop-shadow-lg"
+                    src="https://uortjlczjmucmpaqqhqm.supabase.co/storage/v1/object/public/firejet-converted-images/images/547ac2f119d2d814e73a0bbc3059198323006a05.webp"
+                   />
+                  <img
+                    className="absolute bottom-8 right-96 h-60 w-36 object-cover object-center drop-shadow-lg"
+                    src="https://uortjlczjmucmpaqqhqm.supabase.co/storage/v1/object/public/firejet-converted-images/images/aedc3a182ea425dad6521b1d38f98f0f45f93bab.webp"
+                   />
+                  <img
+                    className="absolute bottom-11 left-32 h-60 w-40 object-cover object-center drop-shadow-lg"
+                    src="https://uortjlczjmucmpaqqhqm.supabase.co/storage/v1/object/public/firejet-converted-images/images/9326acb093602d40efb9d3c2fd69dee3519f7f9d.webp"
+                   />
+                </div>
+              </div>
+              <div className="ml-16 mt-11 flex w-60 items-end justify-end">
+                Best Sellers
+              </div>
+            </div>
+            <div className="absolute bottom-0 left-56 flex h-96 w-80 flex-col items-center justify-between gap-y-4 rounded-[60px] bg-orange-200 px-6 pb-5 pt-3.5 text-xl leading-[normal] text-white drop-shadow-lg" >
+              <div className="flex items-center justify-center self-stretch rounded-[60px] bg-neutral-300/[0.27] py-12 px-24 drop-shadow-lg" >
+                <img
+                  className="h-48 w-24 object-cover object-center drop-shadow-lg"
+                  src="https://uortjlczjmucmpaqqhqm.supabase.co/storage/v1/object/public/firejet-converted-images/images/078f209b2e2d777c4b590d074782ec23ca8a1155.webp"
+                 />
+              </div>
+              <div className="flex items-end justify-end self-stretch rounded-[60px] bg-neutral-300/[0.19] pb-9 pl-12 pr-10 pt-11 drop-shadow-lg" >
+                <div className="w-52">
+                  Hair Type Recc
+                  <br />
+                  {"Ingredients "}
+                  <br />
+                  Price
+                  <br />
+                  Is it popular?
+                </div>
+              </div>
+            </div>
+            <div className="absolute bottom-0 left-96 flex h-96 w-80 flex-col items-center justify-between gap-y-4 rounded-[60px] bg-orange-200 pb-5 pl-6 pr-7 pt-3.5 text-xl leading-[normal] text-white drop-shadow-lg" >
+              <div className="flex items-center justify-center self-stretch rounded-[60px] bg-neutral-300/[0.27] py-12 px-24 drop-shadow-lg" >
+                <img
+                  className="h-48 w-24 object-cover object-center drop-shadow-lg"
+                  src="https://uortjlczjmucmpaqqhqm.supabase.co/storage/v1/object/public/firejet-converted-images/images/078f209b2e2d777c4b590d074782ec23ca8a1155.webp"
+                 />
+              </div>
+              <div className="flex items-end justify-end self-stretch rounded-[60px] bg-neutral-300/[0.19] pb-7 pl-20 pr-2 pt-14 drop-shadow-lg" >
+                <div className="w-52">
+                  Hair Type Recc
+                  <br />
+                  {"Ingredients "}
+                  <br />
+                  Price
+                  <br />
+                  Is it popular?
+                </div>
+              </div>
+            </div>
+            <div className="absolute bottom-0 right-60 flex h-96 w-80 flex-col items-center justify-between gap-y-4 rounded-[60px] bg-orange-200 px-6 pb-5 pt-3.5 text-xl leading-[normal] text-white drop-shadow-lg" >
+              <div className="flex items-center justify-center self-stretch rounded-[60px] bg-neutral-300/[0.27] py-12 px-24 drop-shadow-lg" >
+                <img
+                  className="h-48 w-24 object-cover object-center drop-shadow-lg"
+                  src="https://uortjlczjmucmpaqqhqm.supabase.co/storage/v1/object/public/firejet-converted-images/images/078f209b2e2d777c4b590d074782ec23ca8a1155.webp"
+                 />
+              </div>
+              <div className="flex items-end justify-end self-stretch rounded-[60px] bg-neutral-300/[0.19] pb-9 pl-20 pr-2.5 pt-11 drop-shadow-lg" >
+                <div className="w-52">
+                  Hair Type Recc
+                  <br />
+                  {"Ingredients "}
+                  <br />
+                  Price
+                  <br />
+                  Is it popular?
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-0 items-center pr-0">
+          <div className="ml-[-879px] flex w-24 items-start justify-center self-stretch pb-96" >
+            <div className="bg-more h-24 w-24 bg-contain bg-center bg-no-repeat" />
+          </div>
+        </div>
+        <div className="flex w-0 items-center pr-0 text-[40px] leading-[normal] text-yellow-800" >
+          <div className="ml-[-922px] flex w-44 items-start justify-center self-stretch pb-96" >
+            Hair Type
+          </div>
+        </div>
+        <div className="flex w-0 items-center pr-0">
+          <div className="ml-[-1425px] flex w-[1192px] flex-wrap items-start justify-center gap-y-20 gap-x-20 self-stretch" >
+            <div className="flex items-center justify-center rounded-[60px] bg-orange-200 py-8 px-6 drop-shadow-lg" >
+              <div className="h-80 w-72 rounded-[60px] bg-neutral-300/[0.27] drop-shadow-lg" />
+            </div>
+            <div className="flex flex-col items-center justify-between gap-y-3 self-stretch" >
+              <div className="flex items-center justify-center self-stretch rounded-[60px] bg-orange-200 py-8 px-6 drop-shadow-lg" >
+                <div className="h-80 flex-grow rounded-[60px] bg-neutral-300/[0.27] drop-shadow-lg" />
+              </div>
+              <div className="flex items-center pr-1">
+                <div className="bg-more h-24 w-24 bg-contain bg-center bg-no-repeat" />
+              </div>
+            </div>
+            <div className="flex items-center justify-center rounded-[60px] bg-orange-200 py-8 px-6 drop-shadow-lg" >
+              <div className="h-80 w-72 rounded-[60px] bg-neutral-300/[0.27] drop-shadow-lg" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+interface Desktop1Props {
+  className?: string;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./**/*.{js,jsx,ts,tsx,html,svelte,vue}"],
+  theme: {
+    extend: {
+      fontFamily: { josefin_sans: "Josefin Sans" },
+      backgroundImage: {
+        "b-bg-ellipse-7bbpolygon-1bbpolygon-2":
+          'url("/assets/Ellipse7.svg")',
+        more: "url(https://uortjlczjmucmpaqqhqm.supabase.co/storage/v1/object/public/firejet-converted-images/images/9bb1e17d4fb8d2407a1dfdc882ce2bc99e8cd119.webp)",
+      },
+    },
+  },
+  plugins: [],
+  mode: "jit",
+};
