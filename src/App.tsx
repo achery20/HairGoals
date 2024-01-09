8ba1b2d13b0cdc7d708df24a951be9eff58b842e
achery20 <dyann.chery@gmail.com>
2024-01-09 15:55:26 -0500

    Your commit message here

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Desktop1 from "./components/Desktop1";
+import Signup from "./components/Signup";
 
 function App() {
   return <Desktop1 />;
+  return <Signup />;
 }
 
 export default App;
